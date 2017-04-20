a1c1b7f3b52948df0fee2cafadc62d92d360df57
SuwonC <zx3211226@naver.com>
2017-04-20 17:38:29 +0900

    Create printX.c

---
+++ 2017-04-20/printX.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int i, j,k,l,m;
+	for (i = 1; i <= 9; i ++) {
+		k = 5-i;
+		l = i-5;
+		for (m = -4; m <= 4; m ++) {
+			if (k == m || l == m) printf("#");
+			else printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
