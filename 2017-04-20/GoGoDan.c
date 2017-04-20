53c219b13233fdcc48a165a71bb371fc9ab72ed9
SuwonC <zx3211226@naver.com>
2017-04-20 18:10:54 +0900

    Create GoGoDan.c

---
+++ 2017-04-20/GoGoDan.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int i, j, k;
+	printf("숫자를 입력해주세요. : ");
+	scanf("%d", &i);
+	printf("\n");
+	for (j = 1; j <= i; j ++) {
+		if (j%2 == i%2) {
+			printf("=-=-=-=-= %d단 =-=-=-=-=\n", j);
+			for (k = 1; k <= i; k ++) {
+				printf("%d x %d = %d\n", j, k, j*k);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
