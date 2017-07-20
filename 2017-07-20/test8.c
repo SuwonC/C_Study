7cccd30462d37f0896828abf2332855ccd21cf6e
SuwonC <zx3211226@naver.com>
2017-07-20 17:07:12 +0900

    Update test8.c

--- 2017-07-20/test8.c
+++ 2017-07-20/test8.c
@@ -17,7 +17,7 @@ int main(void) {
 	info.age = tempi;
 	printf("성별을 입력하세요 (남/여) : ");
 	scanf("%s", &tempc);
-	info.gender = strcmp(tempc, "여") == 0 ? 0 : 1;
+	info.gender = (strcmp(tempc, "여") == 0 || strcmp(tempc, "W") == 0 || strcmp(tempc, "G") == 0 || strcmp(tempc, "女") == 0) ? 0 : 1;
 
 	printf("당신의 이름은 %s입니다.\n", info.name);
 	printf("당신의 나이는 %d입니다.\n", info.age);
