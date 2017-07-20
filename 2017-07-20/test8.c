89fe5c9409a6326eb8f29b256ac986bc3f742c28
SuwonC <zx3211226@naver.com>
2017-07-20 17:10:19 +0900

    Update test8.c

--- 2017-07-20/test8.c
+++ 2017-07-20/test8.c
@@ -8,17 +8,16 @@ typedef struct personInfo {
 
 int main(void) {
 	PersonInfo info = { "", 0, 0 };
-	int tempi;
 	char tempc[20];
 	printf("이름을 입력하세요 : ");
 	gets(info.name);
 	printf("나이를 입력하세요 : ");
-	scanf("%d", &tempi);
-	info.age = tempi;
+	scanf("%d", &info.age);
 	printf("성별을 입력하세요 (남/여) : ");
 	scanf("%s", &tempc);
 	info.gender = (strcmp(tempc, "여") == 0 || strcmp(tempc, "W") == 0 || strcmp(tempc, "G") == 0 || strcmp(tempc, "女") == 0) ? 0 : 1;
-
+	system("cls");//window
+	//system("clear");//linux
 	printf("당신의 이름은 %s입니다.\n", info.name);
 	printf("당신의 나이는 %d입니다.\n", info.age);
 	printf("당신의 성별은 %s입니다.\n", info.gender == 0 ? "여" : "남");
